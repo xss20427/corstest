6dd6e8f9aec97b6d5975bf1bd163383a298ca8a1
yisi-xss <xss20427@sina.cn>
2020-01-03 10:33:48 +0800

    Initial commit

---
+++ src/main/java/cn/wzvtc/soft/dao/LvliRepository.java
@@ -0,0 +1,8 @@
+package cn.wzvtc.soft.dao;
+
+import cn.wzvtc.soft.entity.Lvli;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LvliRepository  extends JpaRepository<Lvli, Long> {
+
+}
